c266c78a3ab1e62c7b80aa251ec37d2660c11626
Artrediss <canurul@sabanciuniv.edu>
2023-12-11 18:56:11 +0300

    completed

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { formatDistanceToNow, differenceInDays } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
+  const diff = differenceInDays(new Date(taskObj.deadline), new Date());
+  const bgColor = diff < 3 ? "bg-red-100" : "bg-blue-200"
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim:
+         <span className={bgColor}>
+          {Math.abs(diff)} gün 
+          {diff > 0 ? " önce" : " sonra"}
+         </span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
-        {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+        {taskObj.people.map((p) => (-
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
